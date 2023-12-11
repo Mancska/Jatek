aa72b59ed8b4bdf127ead01f60a0e45d69f2be44
Mancska <95695531+Mancska@users.noreply.github.com>
2023-12-11 11:38:22 +0100

    asd

---
+++ src/jatek/Targy.java
@@ -0,0 +1,54 @@
+
+package jatek;
+
+
+class Targy {
+    String nev, leiras,id;
+
+    public Targy(String nev, String leiras, String id) {
+        this.nev = nev;
+        this.leiras = leiras;
+        this.id = id;
+    }
+    public void hasznal(String szoveg){
+        
+      
+        
+    }
+    
+     public void hasznal(String szoveg, int db){
+        
+      
+        
+    }
+     
+      public void hasznal(String szoveg, int db,boolean eleg){
+        
+      
+        
+    }
+    public String getNev() {
+        return nev;
+    }
+
+    public String getLeiras() {
+        return leiras;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setNev(String nev) {
+        this.nev = nev;
+    }
+
+    public void setLeiras(String leiras) {
+        this.leiras = leiras;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+}
--- src/jatek/karakter.java
+++ src/jatek/karakter.java
@@ -1,8 +1,42 @@
 
 package jatek;
 
+import java.util.List;
+import java.util.Random;
+
 public class karakter {
     int HP;
     int DEX;
     int LUCK;
+    List Items;
+    public Random roll =new Random();
+
+    public karakter(int HP, int DEX, int LUCK, List Items) {
+        this.HP = HP;
+        this.DEX = DEX;
+        this.LUCK = LUCK;
+        this.Items = Items;
+    }
+
+    public void setHP(int HP) {
+        this.HP = HP = (roll.nextInt(6)+1)+(roll.nextInt(6)+1)+12;
+    }
+
+    public void setDEX(int DEX) {
+        this.DEX = DEX = (roll.nextInt(6)+1)+6;
+    }
+
+    public void setLUCK(int LUCK) {
+        this.LUCK = LUCK=(roll.nextInt(6)+1)+6;
+    }
+
+    public void setItems(List Items) {
+        this.Items = Items;
+    }
+
+    public void setRoll(Random roll) {
+        this.roll = roll;
+    }
+    
+
 }
